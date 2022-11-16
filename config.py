7cc71e563d1bb607578f3e29591147cda56e4442
Yuxin <yuxinwangcs@outlook.com>
2022-11-15 20:50:20 -0800

    .

---
+++ config.py
@@ -0,0 +1,9 @@
+
+def init_configures():
+    alpha_ = [0.3]
+    beta_ = [0.8]
+    freq_pen = [0.5]
+    prompt_model = ['text-curie-001']
+    story_model = ['text-curie-001']
+
+    return alpha_, beta_, freq_pen, prompt_model, story_model
